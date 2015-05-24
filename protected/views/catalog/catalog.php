323c41809cc775101be34065938c1d8aaf02a790
opennota <opennota@gmail.com>
2015-05-24 14:57:42 +0700

    Remove dead code from the catalog view

--- protected/views/catalog/catalog.php
+++ protected/views/catalog/catalog.php
@@ -52,28 +52,3 @@
 </p>
 
 <?php endif; ?>
-
-<?php if (false && $books_dp): ?>
-<?php
-    if ($books_dp->totalItemCount) {
-        $books = $books_dp->data;
-
-        echo '<h3>'.Yii::t('app', '{n} перевод|{n} перевода|{n} переводов', $books_dp->totalItemCount).'</h3>';
-        $this->widget('bootstrap.widgets.TbPager', ['pages' => $books_dp->pagination]);
-        echo "<ul class='booklist has-icons'>";
-        foreach ($books as $book) {
-            echo '<li>';
-            echo "<i class='ac_read {$book->ac_read}'></i><i class='ac_gen {$book->ac_gen}'></i><i class='ac_tr {$book->ac_tr}'></i> ";
-            echo "{$book->ready} ";
-            echo "{$book->ahref} ";
-            echo Yii::app()->langs->from_to($book->s_lang, $book->t_lang).' ';
-            echo '</li>';
-        }
-        echo '</ul>';
-        $this->widget('bootstrap.widgets.TbPager', ['pages' => $books_dp->pagination]);
-    } else {
-        echo "<div class='alert alert-block'>В этом разделе пока ещё нет ни одного перевода.</div>";
-    }
-?>
-
-<?php endif; ?>
