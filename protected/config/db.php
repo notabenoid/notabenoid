cccdc2ee0dc8af636b994281f9f5be2b0d0e56aa
opennota <opennota@gmail.com>
2015-05-24 12:38:21 +0700

    Move db config and params to separate files

---
+++ protected/config/db.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'connectionString' => 'pgsql:host=localhost;dbname=notabenoid',
+    'username' => 'notabenoid',
+    'password' => '',
+    'charset' => 'utf8',
+    'emulatePrepare' => true,
+    'enableProfiling' => true,
+    'schemaCachingDuration' => 6 * 60 * 60,
+    'queryCacheID' => 'cache',
+];
