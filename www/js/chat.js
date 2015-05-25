e8d391d936549740f1cc944a4f109f8a9c69d1e3
opennota <opennota@gmail.com>
2015-05-25 19:49:16 +0700

    Format .js files

--- www/js/chat.js
+++ www/js/chat.js
@@ -1,175 +1,186 @@
 var Chat = {
-	room_id: 0,
+    room_id: 0,
 
-	box: null,
-	room: null,
+    box: null,
+    room: null,
 
-	timer: null,
-	initialized: false,
-	on: false,
+    timer: null,
+    initialized: false,
+    on: false,
 
-	since: 0,
-	servertime: 0,
-	_tickerTimer: null,
+    since: 0,
+    servertime: 0,
+    _tickerTimer: null,
 
-	init: function() {
-		var $resizer = $("#chat-resizer"), $form = $("#chat-say");
+    init: function() {
+        var $resizer = $("#chat-resizer"),
+            $form = $("#chat-say");
 
-		Chat.box = $("#chat-box");
-		Chat.room = $("#chat-room");
+        Chat.box = $("#chat-box");
+        Chat.room = $("#chat-room");
 
-		if(Chat.box.length == 0 || Chat.room.length == 0) return false;
+        if (Chat.box.length == 0 || Chat.room.length == 0) return false;
 
-		$resizer.mousedown(function(e) {
-			var startY = e.pageY, startHeight = Chat.box.height(), height = startHeight;
+        $resizer.mousedown(function(e) {
+            var startY = e.pageY,
+                startHeight = Chat.box.height(),
+                height = startHeight;
 
-			function mouseMove(e) {
-				e.preventDefault();
-				height = startHeight + startY - e.pageY;
-				if(height > 45) Chat.box.height(height);
-			}
+            function mouseMove(e) {
+                e.preventDefault();
+                height = startHeight + startY - e.pageY;
+                if (height > 45) Chat.box.height(height);
+            }
 
-			function mouseUp(e) {
-				$(document).unbind("mousemove", mouseMove).unbind("mouseup", mouseUp);
-				$("body").removeClass("no-select");
-				Chat.room.scrollTop(65535);
-				User.ini_set("chat.h", height);
-			}
+            function mouseUp(e) {
+                $(document).unbind("mousemove", mouseMove).unbind("mouseup", mouseUp);
+                $("body").removeClass("no-select");
+                Chat.room.scrollTop(65535);
+                User.ini_set("chat.h", height);
+            }
 
-			$("body").addClass("no-select");
-			$(document).bind("mousemove", mouseMove).bind("mouseup", mouseUp);
-		}).bind("dragstart", function(e) {
-				e.preventDefault();
-				return false;
-			});
+            $("body").addClass("no-select");
+            $(document).bind("mousemove", mouseMove).bind("mouseup", mouseUp);
+        }).bind("dragstart", function(e) {
+            e.preventDefault();
+            return false;
+        });
 
-		if(typeof Book != "undefined" && Book.id != 0) {
-			Chat.room_id = Book.id;
-			$form.attr("action", "/chat/room/" + Chat.room_id);
-		}
+        if (typeof Book != "undefined" && Book.id != 0) {
+            Chat.room_id = Book.id;
+            $form.attr("action", "/chat/room/" + Chat.room_id);
+        }
 
 
-		$form.ajaxForm({
-			dataType: "json",
-			beforeSerialize: function($form) {
-				$form.find("[name=since]").val(Chat.since);
-			},
-			beforeSubmit: function() {
-				$form.find("[name=msg]").attr("disabled", true);
-			},
-			success: function(data) {
-				Chat._handleData(data);
+        $form.ajaxForm({
+            dataType: "json",
+            beforeSerialize: function($form) {
+                $form.find("[name=since]").val(Chat.since);
+            },
+            beforeSubmit: function() {
+                $form.find("[name=msg]").attr("disabled", true);
+            },
+            success: function(data) {
+                Chat._handleData(data);
 
-				$form.find("[name=msg]").attr("disabled", false).val("").focus();
-			}
-		});
+                $form.find("[name=msg]").attr("disabled", false).val("").focus();
+            }
+        });
 
-		$form.find(".stop").click(Chat.stop);
+        $form.find(".stop").click(Chat.stop);
 
-		Chat.room.delegate("a.user", "click", function(e) {
-			e.preventDefault();
-			var $input = $form.find("[name=msg]"), nick = $(this).text();
-			$input.val(nick + ", " + $input.val()).focus();
-		});
+        Chat.room.delegate("a.user", "click", function(e) {
+            e.preventDefault();
+            var $input = $form.find("[name=msg]"),
+                nick = $(this).text();
+            $input.val(nick + ", " + $input.val()).focus();
+        });
 
-		Chat.initialized = true;
+        Chat.initialized = true;
 
-		return true;
-	},
-	start: function() {
-		if(!Chat.initialized) if(!Chat.init()) return false;
+        return true;
+    },
+    start: function() {
+        if (!Chat.initialized)
+            if (!Chat.init()) return false;
 
-		Chat.box.show(100);
-		Chat.room.scrollTop(65535);
+        Chat.box.show(100);
+        Chat.room.scrollTop(65535);
 
-		function refresh() {
-			var url = "/chat/room/" + Chat.room_id + "?since=" + Chat.since;
-			$.getJSON(url, Chat._handleData);
-		}
+        function refresh() {
+            var url = "/chat/room/" + Chat.room_id + "?since=" + Chat.since;
+            $.getJSON(url, Chat._handleData);
+        }
 
-		refresh();
-		Chat.timer = Visibility.every(20 * 1000, refresh);
+        refresh();
+        Chat.timer = Visibility.every(20 * 1000, refresh);
 
-		Chat.on = true;
-		User.ini_set("chat.on", 1);
-		Chat.onStart();
+        Chat.on = true;
+        User.ini_set("chat.on", 1);
+        Chat.onStart();
 
-		return true;
-	},
-	stop: function() {
-		Visibility.stop(Chat.timer);
-		Chat.box.hide(100);
-		Chat.on = false;
-		User.ini_set("chat.on", 0);
-		Chat.onStop();
-	},
-	toggle: function() {
-		if(Chat.on) Chat.stop();
-		else Chat.start();
-		return false;
-	},
+        return true;
+    },
+    stop: function() {
+        Visibility.stop(Chat.timer);
+        Chat.box.hide(100);
+        Chat.on = false;
+        User.ini_set("chat.on", 0);
+        Chat.onStop();
+    },
+    toggle: function() {
+        if (Chat.on) Chat.stop();
+        else Chat.start();
+        return false;
+    },
 
-	onStart: function() {},
-	onStop: function() {},
+    onStart: function() {},
+    onStop: function() {},
 
-	_handleData: function(data) {
-		Chat.since = data.servertime;
-		if(data.room.length) {
-			for(var i in data.room) {
-				Chat.room.append(Chat._renderLine(data.room[i]));
-			}
-			Chat.room.scrollTop(65535);
-		}
-		Chat.servertime = data.servertime;
-		if(Chat._tickerTimer === null) Chat._tickerTimer = setInterval(Chat._ticker, 10000);
-	},
-	_ticker: function() {
-		Chat.servertime += 10;
+    _handleData: function(data) {
+        Chat.since = data.servertime;
+        if (data.room.length) {
+            for (var i in data.room) {
+                Chat.room.append(Chat._renderLine(data.room[i]));
+            }
+            Chat.room.scrollTop(65535);
+        }
+        Chat.servertime = data.servertime;
+        if (Chat._tickerTimer === null) Chat._tickerTimer = setInterval(Chat._ticker, 10000);
+    },
+    _ticker: function() {
+        Chat.servertime += 10;
 
-		Chat.room.find("p").each(function() {
-			var $p = $(this), $a = $p.find("a.user");
-			$a.attr("title", Chat._ago(Chat.servertime - $p.data("time")));
-		});
-	},
-	_renderLine: function(data) {
-		var d = new Date(parseInt(data.t) * 1000),
-			date = Chat._ago(Chat.since - data.t); // d.toLocaleString();
-		return "<p" + (data.i == User.id ? " class='my'" : "") + " data-time='" + data.t + "'><a href='/users/" + data.i + "' class='user' title='" + date + "'>" + data.u + "</a>: " + data.m + "</p>";
-	},
-	_rus: function(n, t1, t2, t5) {
+        Chat.room.find("p").each(function() {
+            var $p = $(this),
+                $a = $p.find("a.user");
+            $a.attr("title", Chat._ago(Chat.servertime - $p.data("time")));
+        });
+    },
+    _renderLine: function(data) {
+        var d = new Date(parseInt(data.t) * 1000),
+            date = Chat._ago(Chat.since - data.t); // d.toLocaleString();
+        return "<p" + (data.i == User.id ? " class='my'" : "") + " data-time='" + data.t + "'><a href='/users/" + data.i + "' class='user' title='" + date + "'>" + data.u + "</a>: " + data.m + "</p>";
+    },
+    _rus: function(n, t1, t2, t5) {
 
-	},
-	_ago: function(time) {
-		if(time <= 0) return "только что";
+    },
+    _ago: function(time) {
+        if (time <= 0) return "только что";
 
-		var t = "", days = 0, d = 0, h = 0, m = 0, s = 0;
-		s = time % 60;
-		m = Math.floor(time / 60);
-		if(m > 60) {
-			h = Math.floor(m / 60);
-			m = m % 60;
-			if(h > 24) {
-				days = Math.floor(h / 24);
-				h = h % 24;
-			}
-		}
-		s = (Math.round(s / 10) * 10);
+        var t = "",
+            days = 0,
+            d = 0,
+            h = 0,
+            m = 0,
+            s = 0;
+        s = time % 60;
+        m = Math.floor(time / 60);
+        if (m > 60) {
+            h = Math.floor(m / 60);
+            m = m % 60;
+            if (h > 24) {
+                days = Math.floor(h / 24);
+                h = h % 24;
+            }
+        }
+        s = (Math.round(s / 10) * 10);
 
-		if(days > 0) t += days + " сут. ";
-		if(h > 0) t += h + " час. ";
-		if(m != 0) t += m + " мин. ";
-		if(h == 0 && m == 0) t += s + " сек. ";
-		t += "тому назад";
+        if (days > 0) t += days + " сут. ";
+        if (h > 0) t += h + " час. ";
+        if (m != 0) t += m + " мин. ";
+        if (h == 0 && m == 0) t += s + " сек. ";
+        t += "тому назад";
 
-		return t;
-	}
+        return t;
+    }
 };
 
 $(function() {
-	$(document).keydown(function(e) {
-		if(e.ctrlKey && (e.which == 192 || e.which == 0)) {
-			Chat.toggle();
-		}
-	});
-	if(User.ini_get("chat.on") == 1) Chat.start();
+    $(document).keydown(function(e) {
+        if (e.ctrlKey && (e.which == 192 || e.which == 0)) {
+            Chat.toggle();
+        }
+    });
+    if (User.ini_get("chat.on") == 1) Chat.start();
 });
--- www/js/mail.js
+++ www/js/mail.js
@@ -1,28 +1,30 @@
 (function() {
-	var $massForm = $("#form-mass"), $checkAll = $("#cb-check-all"), $checkboxes = $massForm.find("[name=id\\[\\]]"),
-		$massActions = $("#mass-actions");
+    var $massForm = $("#form-mass"),
+        $checkAll = $("#cb-check-all"),
+        $checkboxes = $massForm.find("[name=id\\[\\]]"),
+        $massActions = $("#mass-actions");
 
-	function massNeeded() {
-		if($checkboxes.filter(":checked").length) {
-			$massActions.show();
-		} else {
-			$massActions.hide();
-			$checkAll.prop("checked", false);
-		}
-	}
+    function massNeeded() {
+        if ($checkboxes.filter(":checked").length) {
+            $massActions.show();
+        } else {
+            $massActions.hide();
+            $checkAll.prop("checked", false);
+        }
+    }
 
-	$checkAll.click(function(e) {
-		$checkboxes.prop("checked", $checkAll.prop("checked"));
-		massNeeded();
-	});
+    $checkAll.click(function(e) {
+        $checkboxes.prop("checked", $checkAll.prop("checked"));
+        massNeeded();
+    });
 
-	$checkboxes.click(massNeeded);
+    $checkboxes.click(massNeeded);
 
-	$massForm.submit(function(e) {
-		if($massForm.find("[name=act]").val() == "rm") {
-			var n = $checkboxes.filter(":checked").length;
-			return confirm("Вы уверены, что хотите удалить выбранные сообщения (" + n + "шт.) ?");
-		}
-		return true;
-	});
+    $massForm.submit(function(e) {
+        if ($massForm.find("[name=act]").val() == "rm") {
+            var n = $checkboxes.filter(":checked").length;
+            return confirm("Вы уверены, что хотите удалить выбранные сообщения (" + n + "шт.) ?");
+        }
+        return true;
+    });
 })();
