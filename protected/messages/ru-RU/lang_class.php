374ec74d330c7ac31ff6c80f4d3d878a20744ece
opennota <opennota@gmail.com>
2015-05-30 11:41:17 +0700

    Add translation files

---
+++ protected/messages/ru-RU/lang_class.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'Africa' => 'Африка',
+    'America' => 'Америка',
+    'Asia' => 'Азия',
+    'Australia and Oceania' => 'Австралия и Океания',
+    'Europe' => 'Европа',
+    'Other' => 'Прочие',
+    'Principal' => 'Основные',
+    'The peoples of the ex-USSR' => 'Народы СССР',
+];
